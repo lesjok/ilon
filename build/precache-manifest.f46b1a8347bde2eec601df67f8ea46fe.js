904d99db85d68607ff31bf071ebcbfdf986a629e
lesjok <lesjok@inbox.ru>
2023-01-06 19:32:42 +0300

    r

---
+++ build/precache-manifest.f46b1a8347bde2eec601df67f8ea46fe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "fde5ea4a350ece480a1b",
+    "url": "/ilon/static/css/main.7871d177.chunk.css"
+  },
+  {
+    "revision": "fde5ea4a350ece480a1b",
+    "url": "/ilon/static/js/main.fde5ea4a.chunk.js"
+  },
+  {
+    "revision": "f60d88dc5026629ed6c1",
+    "url": "/ilon/static/css/1.9dca1eca.chunk.css"
+  },
+  {
+    "revision": "f60d88dc5026629ed6c1",
+    "url": "/ilon/static/js/1.f60d88dc.chunk.js"
+  },
+  {
+    "revision": "04ee0de7a29e586b94aa",
+    "url": "/ilon/static/js/runtime~main.04ee0de7.js"
+  },
+  {
+    "revision": "45da66b55a52320b55a7bb5ed461667c",
+    "url": "/ilon/static/media/menu-icon.45da66b5.svg"
+  },
+  {
+    "revision": "945c4c2af23d280c1c37b055043896bb",
+    "url": "/ilon/static/media/background.945c4c2a.jpg"
+  },
+  {
+    "revision": "e6d20f342072046be71d552f603012a9",
+    "url": "/ilon/static/media/background-with-title.e6d20f34.jpg"
+  },
+  {
+    "revision": "29186d20e4496cc3ce57e238a2572522",
+    "url": "/ilon/index.html"
+  }
+];
